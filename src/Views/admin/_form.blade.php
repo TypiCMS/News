e19dccba45375291dcc32dd25b745e3688ce7255
Samuel De Backer <sdebacker@gmail.com>
2015-02-07 10:22:45 +0100

    include

--- src/Views/admin/_form.blade.php
+++ src/Views/admin/_form.blade.php
@@ -42,21 +42,7 @@
         @foreach ($locales as $lang)
 
             <div class="tab-pane fade @if($locale == $lang)in active @endif" id="content-{{ $lang }}">
-                <div class="row">
-                    <div class="col-md-6 form-group">
-                        {!! BootForm::text(trans('validation.attributes.title'), $lang.'[title]') !!}
-                    </div>
-                    <div class="col-md-6 form-group @if($errors->has($lang.'.slug'))has-error @endif">
-                        <label class="control-label" for="{{ $lang }}[slug]">@lang('validation.attributes.slug')</label>
-                        <div class="input-group">
-                            <input class="form-control" type="text" name="{{ $lang }}[slug]" id="{{ $lang }}[slug]" value="@if($model->hasTranslation($lang)){{ $model->translate($lang)->slug }}@endif">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default btn-slug @if($errors->has($lang.'.slug'))btn-danger @endif" type="button">@lang('validation.attributes.generate')</button>
-                            </span>
-                        </div>
-                        {!! $errors->first($lang.'.slug', '<p class="help-block">:message</p>') !!}
-                    </div>
-                </div>
+                @include('core::admin._title-and-slug')
                 {!! BootForm::checkbox(trans('validation.attributes.online'), $lang.'[status]') !!}
                 {!! BootForm::textarea(trans('validation.attributes.summary'), $lang.'[summary]') !!}
                 {!! BootForm::textarea(trans('validation.attributes.body'), $lang.'[body]')->addClass('editor') !!}
