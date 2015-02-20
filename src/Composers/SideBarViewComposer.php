cf140375bb4262385206f1fea46f0fd9e4aa5304
Samuel De Backer <sdebacker@gmail.com>
2015-02-20 01:32:12 +0100

    licence + sidebar

--- src/Composers/SideBarViewComposer.php
+++ src/Composers/SideBarViewComposer.php
@@ -1,18 +1,25 @@
 <?php
 namespace TypiCMS\Modules\News\Composers;
 
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
+use TypiCMS\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->menus['content']->put('news', [
-            'weight' => config('typicms.news.sidebar.weight'),
-            'request' => $view->prefix . '/news*',
-            'route' => 'admin.news.index',
-            'icon-class' => 'icon fa fa-fw fa-bullhorn',
-            'title' => 'News',
-        ]);
+        $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
+            $group->addItem(trans('news::global.name'), function (SidebarItem $item) {
+                $item->icon = config('typicms.news.sidebar.icon', 'icon fa fa-fw fa-bullhorn');
+                $item->weight = config('typicms.news.sidebar.weight');
+                $item->route('admin.news.index');
+                $item->append('admin.news.create');
+                $item->authorize(
+                    $this->auth->hasAccess('news.index')
+                );
+            });
+        });
     }
 }
