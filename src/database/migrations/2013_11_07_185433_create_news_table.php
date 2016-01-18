08ce913da91ddc0dae8e33217a072a6c1d72c276
Samuel De Backer <sdebacker@gmail.com>
2016-01-18 21:45:48 +0100

    slug unique

--- src/database/migrations/2013_11_07_185433_create_news_table.php
+++ src/database/migrations/2013_11_07_185433_create_news_table.php
@@ -14,35 +14,26 @@ class CreateNewsTable extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->date('date');
             $table->string('image')->nullable();
-
             $table->timestamps();
         });
 
         Schema::create('news_translations', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->integer('news_id')->unsigned();
-
             $table->string('locale')->index();
-
             $table->boolean('status')->default(0);
-
             $table->string('title');
             $table->string('slug')->nullable();
-
             $table->text('summary');
             $table->text('body');
-
             $table->timestamps();
-
             $table->unique(['news_id', 'locale']);
+            $table->unique(['locale', 'slug']);
             $table->foreign('news_id')->references('id')->on('news')->onDelete('cascade');
-
         });
     }
 
