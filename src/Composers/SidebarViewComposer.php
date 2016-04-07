ffa6429031161e12256aec513bfcc37acc87b862
Samuel De Backer <sdebacker@gmail.com>
2016-04-07 11:28:44 +0200

    Gate in place of Auth

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -3,6 +3,7 @@
 namespace TypiCMS\Modules\News\Composers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Gate;
 use Maatwebsite\Sidebar\SidebarGroup;
 use Maatwebsite\Sidebar\SidebarItem;
 
@@ -17,7 +18,7 @@ class SidebarViewComposer
                 $item->route('admin::index-news');
                 $item->append('admin::create-news');
                 $item->authorize(
-                    auth()->user()->can('index-news')
+                    Gate::allows('index-news')
                 );
             });
         });
