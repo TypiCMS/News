0e43e7bd8d2b1f150c438ed595dc8f7440a6462f
Samuel De Backer <sdebacker@gmail.com>
2016-04-15 16:40:30 +0200

    prepare for spatie translatable

--- src/Models/News.php
+++ src/Models/News.php
@@ -4,31 +4,22 @@ namespace TypiCMS\Modules\News\Models;
 
 use Laracasts\Presenter\PresentableTrait;
 use Spatie\Translatable\HasTranslations;
-use Spatie\Translatable\Translatable;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\History\Traits\Historable;
 
 class News extends Base
 {
-    use Historable;
     use HasTranslations;
+    use Historable;
     use PresentableTrait;
 
     protected $presenter = 'TypiCMS\Modules\News\Presenters\ModulePresenter';
 
     protected $dates = ['date'];
 
-    protected $casts = [
-        'title' => 'array',
-        'slug' => 'array',
-        'status' => 'array',
-        'summary' => 'array',
-        'body' => 'array',
-    ];
+    protected $guarded = ['id'];
 
-    protected $fillable = [
-        'date',
-        'image',
+    public $translatable = [
         'title',
         'slug',
         'status',
@@ -36,25 +27,9 @@ class News extends Base
         'body',
     ];
 
-    /**
-     * Columns that are file.
-     *
-     * @var array
-     */
     public $attachments = [
         'image',
     ];
-
-    public function getTranslatableFields()
-    {
-        return [
-            'title',
-            'slug',
-            'status',
-            'summary',
-            'body',
-        ];
-    }
 
     /**
      * A news has many galleries.
@@ -67,26 +42,6 @@ class News extends Base
             ->withPivot('position')
             ->orderBy('position')
             ->withTimestamps();
-    }
-
-    /**
-     * Append status attribute from translation table.
-     *
-     * @return string
-     */
-    public function getStatusAttribute()
-    {
-        return $this->status;
-    }
-
-    /**
-     * Append title attribute from translation table.
-     *
-     * @return string title
-     */
-    public function getTitleAttribute()
-    {
-        return $this->title;
     }
 
     /**
--- src/database/migrations/2013_11_07_185433_create_news_table.php
+++ src/database/migrations/2013_11_07_185433_create_news_table.php
@@ -17,23 +17,12 @@ class CreateNewsTable extends Migration
             $table->increments('id');
             $table->date('date');
             $table->string('image')->nullable();
+            $table->json('status');
+            $table->json('title');
+            $table->json('slug');
+            $table->json('summary');
+            $table->json('body');
             $table->timestamps();
-        });
-
-        Schema::create('news_translations', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->integer('news_id')->unsigned();
-            $table->string('locale')->index();
-            $table->boolean('status')->default(0);
-            $table->string('title');
-            $table->string('slug')->nullable();
-            $table->text('summary');
-            $table->text('body');
-            $table->timestamps();
-            $table->unique(['news_id', 'locale']);
-            $table->unique(['locale', 'slug']);
-            $table->foreign('news_id')->references('id')->on('news')->onDelete('cascade');
         });
     }
 
@@ -44,7 +33,6 @@ class CreateNewsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('news_translations');
         Schema::drop('news');
     }
 }
