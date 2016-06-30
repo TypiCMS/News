22a164553aba4d1f9fa93f577a17cce6eafa8d54
Samuel De Backer <sdebacker@gmail.com>
2016-06-30 14:34:16 +0200

    no translations

--- src/Http/Controllers/PublicController.php
+++ src/Http/Controllers/PublicController.php
@@ -28,7 +28,7 @@ class PublicController extends BasePublicController
     {
         $page = Request::input('page');
         $perPage = config('typicms.news.per_page');
-        $data = $this->repository->byPage($page, $perPage, ['translations']);
+        $data = $this->repository->byPage($page, $perPage);
         $models = new Paginator($data->items, $data->totalItems, $perPage, null, ['path' => Paginator::resolveCurrentPath()]);
 
         return view('news::public.index')
@@ -49,7 +49,7 @@ class PublicController extends BasePublicController
             $feed->setCache(60, 'typicmsNewsFeed');
         }
         if (!$feed->isCached()) {
-            $models = $this->repository->latest(10, ['translations']);
+            $models = $this->repository->latest(10);
 
             $feed->title = $page->title.' – '.TypiCMS::title();
             $feed->description = $page->body;
