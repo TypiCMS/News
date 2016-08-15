e56d91097460baff864d9d87158c6158bb5385f8
Leonardo Lorbicki <leonardosaints@gmail.com>
2016-08-15 10:39:13 -0300

    added portuguese translation

---
+++ src/resources/lang/pt/global.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name'         => 'Notícias',
+    'news'         => 'notícia|notícias',
+    'New'          => 'Nova notícia',
+    'Edit'         => 'Editar notícia',
+    'Back'         => 'Voltar para notícias',
+    'Published on' => 'Publicado em',
+    'feed'         => 'Notícias',
+];
