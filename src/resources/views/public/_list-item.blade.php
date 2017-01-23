e76951f19853d594428efa34f12bbd092fe9e6ed
Samuel De Backer <sdebacker@gmail.com>
2017-01-23 20:16:32 +0100

    default classes

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,13 +1,13 @@
-<li class="news" itemscope itemtype="http://schema.org/Article">
-    <a class="news-anchor" href="{{ route($lang.'::news', $news->slug) }}" itemprop="url">
+<li class="newslist-item" itemscope itemtype="http://schema.org/Article">
+    <a class="newslist-item-anchor" href="{{ route($lang.'::news', $news->slug) }}" itemprop="url">
         {!! $news->present()->thumb(540, 400) !!}
         <meta itemprop="image" content="{{ $news->present()->thumbUrl() }}">
-        <div class="news-info">
-            <h2 class="news-title" itemprop="name">{{ $news->title }}</h2>
-            <div class="news-date-wrapper">@lang('news::global.Published on')
-                <time class="news-date" itemprop="datePublished" datetime="{{ $news->date->toIso8601String() }}">{{ $news->present()->dateLocalized }}</time>
+        <div class="newslist-item-info">
+            <h2 class="newslist-item-title" itemprop="name">{{ $news->title }}</h2>
+            <div class="newslist-item-date-wrapper">@lang('news::global.Published on')
+                <time class="newslist-item-date" itemprop="datePublished" datetime="{{ $news->date->toIso8601String() }}">{{ $news->present()->dateLocalized }}</time>
             </div>
-            <div class="news-summary" itemprop="headline">{{ $news->summary }}</div>
+            <div class="newslist-item-summary" itemprop="headline">{{ $news->summary }}</div>
         </div>
     </a>
 </li>
