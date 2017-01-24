adc64f668a248673312b1993bbcbfd4709f33a94
Samuel De Backer <sdebacker@gmail.com>
2017-01-24 20:00:07 +0100

    th translations

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -23,10 +23,10 @@
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
-                    <th st-sort="status" class="status st-sort">Status</th>
-                    <th st-sort="image" class="image st-sort">Image</th>
-                    <th st-sort="date" st-sort-default="reverse" class="date st-sort">Date</th>
-                    <th st-sort="title" class="title st-sort">Title</th>
+                    <th st-sort="status" class="status st-sort">@lang('Status')</th>
+                    <th st-sort="image" class="image st-sort">@lang('Image')</th>
+                    <th st-sort="date" st-sort-default="reverse" class="date st-sort">@lang('Date')</th>
+                    <th st-sort="title" class="title st-sort">@lang('Title')</th>
                 </tr>
                 <tr>
                     <td colspan="5"></td>
