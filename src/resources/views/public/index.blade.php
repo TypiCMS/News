ab463e1c8295c8ecc9e6d9708b9f18208dae9e55
Samuel De Backer <sdebacker@gmail.com>
2017-06-14 11:52:48 +0200

    includeWhen

--- src/resources/views/public/index.blade.php
+++ src/resources/views/public/index.blade.php
@@ -8,9 +8,7 @@
 
     @include('files::public._files', ['model' => $page])
 
-    @if ($models->count())
-    @include('news::public._list', ['items' => $models])
-    @endif
+    @includeWhen($models->count() > 0, 'news::public._list', ['items' => $models])
 
     {!! $models->appends(Request::except('page'))->links() !!}
 
