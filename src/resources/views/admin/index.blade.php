40020714c1a1ca067661c7459e686eba2fb9061b
Samuel De Backer <sdebacker@gmail.com>
2018-05-16 23:34:40 +0200

    list of news

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,62 +1,61 @@
+@push('js')
+<script>
+
+</script>
+@endpush
+
 @extends('core::admin.master')
 
 @section('title', __('News'))
 
 @section('content')
 
-<div ng-cloak ng-controller="ListController">
+<div id="listItems">
 
     @include('core::admin._button-create', ['module' => 'news'])
 
     <h1>@lang('News')</h1>
 
     <div class="btn-toolbar">
-        @include('core::admin._button-select')
-        @include('core::admin._button-actions')
+        {{-- @include('core::admin._button-select') --}}
+        {{-- @include('core::admin._button-actions') --}}
         @include('core::admin._lang-switcher-for-list')
     </div>
 
     <div class="table-responsive">
 
-        <table st-persist="newsTable" st-table="displayedModels" st-safe-src="models" st-order st-filter class="table table-main">
+        <table class="table table-main">
+
             <thead>
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
-                    <th st-sort="status_translated" class="status st-sort">{{ __('Status') }}</th>
-                    <th st-sort="image" class="image st-sort">{{ __('Image') }}</th>
-                    <th st-sort="date" st-sort-default="reverse" class="date st-sort">{{ __('Date') }}</th>
-                    <th st-sort="title_translated" class="title_translated st-sort">{{ __('Title') }}</th>
-                </tr>
-                <tr>
-                    <td colspan="5"></td>
-                    <td>
-                        <input st-search="title_translated" class="form-control form-control-sm" placeholder="@lang('Filter')…" type="text">
-                    </td>
+                    <th class="status">{{ __('Status') }}</th>
+                    <th class="image">{{ __('Image') }}</th>
+                    <th class="date">{{ __('Date') }}</th>
+                    <th class="title_translated">{{ __('Title') }}</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr ng-repeat="model in displayedModels">
+                <tr v-for="model in models">
                     <td>
-                        <input type="checkbox" checklist-model="checked.models" checklist-value="model">
+                        <input type="checkbox">
                     </td>
                     <td>
                         @include('core::admin._button-edit', ['module' => 'news'])
                     </td>
-                    <td typi-btn-status action="toggleStatus(model)" model="model"></td>
                     <td>
-                        <img ng-src="@{{ model.thumb }}" alt="">
+                        <typi-btn-status :model="model"></typi-btn-status>
                     </td>
-                    <td>@{{ model.date | dateFromMySQL:'dd/MM/yyyy' }}</td>
+                    <td>
+                        <img :src="model.thumb" alt="">
+                    </td>
+                    <td>@{{ model.date }}</td>
                     <td>@{{ model.title_translated }}</td>
                 </tr>
             </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="6" typi-pagination></td>
-                </tr>
-            </tfoot>
+
         </table>
 
     </div>
