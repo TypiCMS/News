7bcdd99e06a269bcf9f72bab62759dcb5499f4ef
Samuel De Backer <sdebacker@gmail.com>
2018-05-23 18:20:13 +0200

    index page

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -12,39 +12,33 @@
 
 <div>
 
-    @include('core::admin._button-create', ['module' => 'news'])
+    <item-list
+        url-base="{{ route('api::index-news') }}"
+        url-parameters="include=files&fields[news]=id,date,status,title&fields[files]=id,path"
+        title="News"
+        :sorting="['-date']">
 
-    <h1>@lang('News')</h1>
-
-    <item-list url="{{ route('admin::index-news') }}">
+        <template slot="add-button">
+            @include('core::admin._button-create', ['module' => 'news'])
+        </template>
 
         <template slot="buttons">
-            @include('core::admin._button-actions')
             @include('core::admin._lang-switcher-for-list')
         </template>
 
-        <template slot="columns">
-            <th class="delete"></th>
-            <th class="edit"></th>
-            <th class="status">{{ __('Status') }}</th>
-            <th class="image">{{ __('Image') }}</th>
-            <th class="date">{{ __('Date') }}</th>
-            <th class="title_translated">{{ __('Title') }}</th>
+        <template slot="columns" slot-scope="{ sortArray }">
+            <column-header name="edit"></column-header>
+            <column-header name="status">Status</column-header>
+            <column-header name="image">Image</column-header>
+            <column-header name="date" sortable :sort-array="sortArray">Date</column-header>
+            <column-header name="title" sortable :sort-array="sortArray">Title</column-header>
         </template>
 
-        <template slot-scope="{ model }">
-            <td>
-                <input type="checkbox">
-            </td>
-            <td>
-                @include('core::admin._button-edit', ['module' => 'news'])
-            </td>
-            <td>
-                <typi-btn-status :model="model"></typi-btn-status>
-            </td>
-            <td>
-                <img :src="model.thumb" alt="">
-            </td>
+        <template slot="table-row" slot-scope="{ model, checkedModels }">
+            <td class="checkbox"><typi-checkbox :model="model" :checked-models-prop="checkedModels"></typi-checkbox></td>
+            <td>@include('core::admin._button-edit', ['module' => 'news'])</td>
+            <td><typi-btn-status :model="model"></typi-btn-status></td>
+            <td><img :src="model.thumb" alt=""></td>
             <td>@{{ model.date | date }}</td>
             <td>@{{ model.title_translated }}</td>
         </template>
