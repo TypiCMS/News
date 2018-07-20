e7506ec607a9df02e7cae980fc8fd91fcde4e53f
Samuel De Backer <sdebacker@gmail.com>
2018-07-20 10:40:23 +0200

    simpler markup

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -10,44 +10,40 @@
 
 @section('content')
 
-<div>
+<item-list
+    url-base="{{ route('api::index-news') }}"
+    locale="{{ config('typicms.content_locale') }}"
+    url-parameters="fields[news]=id,date"
+    title="news"
+    :searchable="['date']"
+    :sorting="['-date']">
 
-    <item-list
-        url-base="{{ route('api::index-news') }}"
-        locale="{{ config('typicms.content_locale') }}"
-        url-parameters="fields[news]=id,date"
-        title="news"
-        :searchable="['date']"
-        :sorting="['-date']">
+    <template slot="add-button">
+        @include('core::admin._button-create', ['module' => 'news'])
+    </template>
 
-        <template slot="add-button">
-            @include('core::admin._button-create', ['module' => 'news'])
-        </template>
+    <template slot="buttons">
+        @include('core::admin._lang-switcher-for-list')
+    </template>
 
-        <template slot="buttons">
-            @include('core::admin._lang-switcher-for-list')
-        </template>
+    <template slot="columns" slot-scope="{ sortArray }">
+        <item-list-column-header name="checkbox"></item-list-column-header>
+        <item-list-column-header name="edit"></item-list-column-header>
+        <item-list-column-header name="status_translated" sortable :sort-array="sortArray" :label="$t('Status')"></item-list-column-header>
+        <item-list-column-header name="image" :label="$t('Image')"></item-list-column-header>
+        <item-list-column-header name="date" sortable :sort-array="sortArray" :label="$t('Date')" filterable></item-list-column-header>
+        <item-list-column-header name="title_translated" sortable :sort-array="sortArray" :label="$t('Title')" filterable></item-list-column-header>
+    </template>
 
-        <template slot="columns" slot-scope="{ sortArray }">
-            <item-list-column-header name="checkbox"></item-list-column-header>
-            <item-list-column-header name="edit"></item-list-column-header>
-            <item-list-column-header name="status_translated" sortable :sort-array="sortArray" :label="$t('Status')"></item-list-column-header>
-            <item-list-column-header name="image" :label="$t('Image')"></item-list-column-header>
-            <item-list-column-header name="date" sortable :sort-array="sortArray" :label="$t('Date')"></item-list-column-header>
-            <item-list-column-header name="title_translated" sortable :sort-array="sortArray" :label="$t('Title')"></item-list-column-header>
-        </template>
+    <template slot="table-row" slot-scope="{ model, checkedModels, loading }">
+        <td class="checkbox"><item-list-checkbox :model="model" :checked-models-prop="checkedModels" :loading="loading"></item-list-checkbox></td>
+        <td>@include('core::admin._button-edit', ['module' => 'news'])</td>
+        <td><item-list-status-button :model="model"></item-list-status-button></td>
+        <td><img :src="model.thumb" alt=""></td>
+        <td>@{{ model.date | date }}</td>
+        <td>@{{ model.title_translated }}</td>
+    </template>
 
-        <template slot="table-row" slot-scope="{ model, checkedModels, loading }">
-            <td class="checkbox"><item-list-checkbox :model="model" :checked-models-prop="checkedModels" :loading="loading"></item-list-checkbox></td>
-            <td>@include('core::admin._button-edit', ['module' => 'news'])</td>
-            <td><item-list-status-button :model="model"></item-list-status-button></td>
-            <td><img :src="model.thumb" alt=""></td>
-            <td>@{{ model.date | date }}</td>
-            <td>@{{ model.title_translated }}</td>
-        </template>
-
-    </item-list>
-
-</div>
+</item-list>
 
 @endsection
