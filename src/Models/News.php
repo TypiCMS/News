972503e5f335393d57b6c9d8c95e6d632fde5545
Samuel De Backer <sdebacker@gmail.com>
2018-10-24 21:14:55 +0200

    getImageAttribute

--- src/Models/News.php
+++ src/Models/News.php
@@ -39,7 +39,7 @@ class News extends Base
      */
     public function getImageAttribute()
     {
-        return $this->files->where('type', 'i')->first();
+        return $this->images()->first();
     }
 
     /**
