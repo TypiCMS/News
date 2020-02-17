07c9c62bd602760ca6e43d90568690092ef26ef8
Samuel De Backer <sdebacker@gmail.com>
2020-02-17 11:27:20 +0100

    @empty in place of @isset

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -4,9 +4,9 @@
         <div class="news-list-item-info">
             <h2 class="news-list-item-title">{{ $news->title }}</h2>
             <div class="news-list-item-date">{{ $news->present()->dateLocalized }}</div>
-            @isset($news->summary)
+            @empty(!$news->summary)
             <div class="news-list-item-summary">{{ $news->summary }}</div>
-            @endisset
+            @endempty
         </div>
     </a>
 </li>
--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -14,16 +14,16 @@
 
     <article class="news">
         <h1 class="news-title">{{ $model->title }}</h1>
-        @isset($model->image)
+        @empty(!$model->image)
         <img class="news-image" src="{!! $model->present()->image(null, 1000) !!}" alt="">
-        @endisset
+        @endempty
         <div class="news-date">{{ $model->present()->dateLocalized }}</div>
-        @isset($model->summary)
+        @empty(!$model->summary)
         <p class="news-summary">{!! nl2br($model->summary) !!}</p>
-        @endisset
-        @isset($model->body)
+        @endempty
+        @empty(!$model->body)
         <div class="news-body">{!! $model->present()->body !!}</div>
-        @endisset
+        @endempty
         @include('files::public._documents')
         @include('files::public._images')
     </article>
