d37b45891f39a7ab16804bc45917ec5ddfcab4b7
Samuel De Backer <sdebacker@gmail.com>
2020-05-20 14:24:59 +0200

    og image size

--- src/resources/views/public/show.blade.php
+++ src/resources/views/public/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', $model->title.' – '.__('News').' – '.$websiteTitle)
 @section('ogTitle', $model->title)
 @section('description', $model->summary)
-@section('image', $model->present()->image())
+@section('image', $model->present()->image(1200, 630))
 @section('bodyClass', 'body-news body-news-'.$model->id.' body-page body-page-'.$page->id)
 
 @section('content')
