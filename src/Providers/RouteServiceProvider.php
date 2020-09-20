4e7540788ff9515748bf0f45e4473f9c701e38e8
Samuel De Backer <sdebacker@gmail.com>
2020-09-20 22:39:26 +0200

    Routes definition cleaned

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -17,48 +17,44 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        Route::namespace($this->namespace)->group(function (Router $router) {
-            /*
-             * Front office routes
-             */
-            if ($page = TypiCMS::getPageLinkedToModule('news')) {
-                $middleware = $page->private ? ['public', 'auth'] : ['public'];
-                $router->middleware($middleware)->group(function (Router $router) use ($page) {
-                    foreach (locales() as $lang) {
-                        if ($page->translate('status', $lang) && $uri = $page->uri($lang)) {
-                            $router->get($uri, [PublicController::class, 'index'])->name($lang.'::index-news');
-                            $router->get($uri.'.xml', [PublicController::class, 'feed'])->name($lang.'::news-feed');
-                            $router->get($uri.'/{slug}', [PublicController::class, 'show'])->name($lang.'::news');
-                        }
-                    }
-                });
+        /*
+         * Front office routes
+         */
+        if ($page = TypiCMS::getPageLinkedToModule('news')) {
+            $middleware = $page->private ? ['public', 'auth'] : ['public'];
+            foreach (locales() as $lang) {
+                if ($page->isPublished($lang) && $uri = $page->uri($lang)) {
+                    Route::middleware($middleware)->prefix($uri)->name($lang.'::')->group(function (Router $router) {
+                        $router->get('/', [PublicController::class, 'index'])->name('index-news');
+                        $router->get('feed.xml', [PublicController::class, 'feed'])->name('news-feed');
+                        $router->get('{slug}', [PublicController::class, 'show'])->name('news');
+                    });
+                }
             }
+        }
 
-            /*
-             * Admin routes
-             */
-            $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('news', [AdminController::class, 'index'])->name('admin::index-news')->middleware('can:read news');
-                $router->get('news/create', [AdminController::class, 'create'])->name('admin::create-news')->middleware('can:create news');
-                $router->get('news/{news}/edit', [AdminController::class, 'edit'])->name('admin::edit-news')->middleware('can:read news');
-                $router->post('news', [AdminController::class, 'store'])->name('admin::store-news')->middleware('can:create news');
-                $router->put('news/{news}', [AdminController::class, 'update'])->name('admin::update-news')->middleware('can:update news');
-            });
+        /*
+         * Admin routes
+         */
+        Route::middleware('admin')->prefix('admin')->name('admin::')->group(function (Router $router) {
+            $router->get('news', [AdminController::class, 'index'])->name('index-news')->middleware('can:read news');
+            $router->get('news/create', [AdminController::class, 'create'])->name('create-news')->middleware('can:create news');
+            $router->get('news/{news}/edit', [AdminController::class, 'edit'])->name('edit-news')->middleware('can:read news');
+            $router->post('news', [AdminController::class, 'store'])->name('store-news')->middleware('can:create news');
+            $router->put('news/{news}', [AdminController::class, 'update'])->name('update-news')->middleware('can:update news');
+        });
 
-            /*
-             * API routes
-             */
-            $router->middleware('api')->prefix('api')->group(function (Router $router) {
-                $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('news', [ApiController::class, 'index'])->middleware('can:read news');
-                    $router->patch('news/{news}', [ApiController::class, 'updatePartial'])->middleware('can:update news');
-                    $router->delete('news/{news}', [ApiController::class, 'destroy'])->middleware('can:delete news');
+        /*
+         * API routes
+         */
+        Route::middleware(['api', 'auth:api'])->prefix('api')->group(function (Router $router) {
+            $router->get('news', [ApiController::class, 'index'])->middleware('can:read news');
+            $router->patch('news/{news}', [ApiController::class, 'updatePartial'])->middleware('can:update news');
+            $router->delete('news/{news}', [ApiController::class, 'destroy'])->middleware('can:delete news');
 
-                    $router->get('news/{news}/files', [ApiController::class, 'files'])->middleware('can:update news');
-                    $router->post('news/{news}/files', [ApiController::class, 'attachFiles'])->middleware('can:update news');
-                    $router->delete('news/{news}/files/{file}', [ApiController::class, 'detachFile'])->middleware('can:update news');
-                });
-            });
+            $router->get('news/{news}/files', [ApiController::class, 'files'])->middleware('can:update news');
+            $router->post('news/{news}/files', [ApiController::class, 'attachFiles'])->middleware('can:update news');
+            $router->delete('news/{news}/files/{file}', [ApiController::class, 'detachFile'])->middleware('can:update news');
         });
     }
 }
