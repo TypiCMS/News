b42006caf0c6a6049d60276024828ee6a281b98f
Samuel De Backer <sdebacker@gmail.com>
2020-09-21 20:51:04 +0200

    Update RouteServiceProvider.php

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -12,9 +12,6 @@ use TypiCMS\Modules\News\Http\Controllers\PublicController;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * Define the routes for the application.
-     */
     public function map()
     {
         /*
