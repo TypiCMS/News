a4056eb4cac2924fb0ea2ffe4b7eb97da1880dbb
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 16:10:30 +0200

    sidebar icon

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -19,7 +19,7 @@ class SidebarViewComposer
             $group->weight = 30;
             $group->addItem(__('News'), function (SidebarItem $item) {
                 $item->id = 'news';
-                $item->icon = config('typicms.news.sidebar.icon', 'icon fa fa-fw fa-bullhorn');
+                $item->icon = config('typicms.news.sidebar.icon');
                 $item->weight = config('typicms.news.sidebar.weight');
                 $item->route('admin::index-news');
                 $item->append('admin::create-news');
