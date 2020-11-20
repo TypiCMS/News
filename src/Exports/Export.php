c4a98dff815f9a5faedbc2ed600abcef505dc554
Samuel De Backer <sdebacker@gmail.com>
2020-11-20 13:58:45 +0100

    Update Export.php

--- src/Exports/Export.php
+++ src/Exports/Export.php
@@ -21,6 +21,7 @@ class Export implements WithColumnFormatting, ShouldAutoSize, FromCollection, Wi
     public function __construct($request)
     {
         $this->collection = QueryBuilder::for(News::class)
+            ->selectFields($request->input('fields.news'))
             ->allowedSorts(['status_translated', 'date', 'title_translated'])
             ->allowedFilters([
                 AllowedFilter::custom('title', new FilterOr()),
