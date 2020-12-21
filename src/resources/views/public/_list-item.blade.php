9fa05a2990d9aa8bd8d458703517c86c275c81a4
Samuel De Backer <sdebacker@gmail.com>
2020-12-21 11:09:55 +0100

    Check empty image + missing alt attribute.

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,6 +1,8 @@
 <li class="news-list-item">
     <a class="news-list-item-link" href="{{ $news->uri() }}">
-        <img class="news-list-item-image" src="{{ $news->present()->image(540, 400) }}" width="{{ $news->image->width }}" height="{{ $news->image->height }}" alt="">
+        @empty (!$news->image)
+        <img class="news-list-item-image" src="{{ $news->present()->image(540, 400) }}" width="{{ $news->image->width }}" height="{{ $news->image->height }}" alt="{{ $news->image->alt_attribute }}">
+        @endempty
         <div class="news-list-item-info">
             <h2 class="news-list-item-title">{{ $news->title }}</h2>
             <div class="news-list-item-date">{{ $news->present()->dateLocalized }}</div>
