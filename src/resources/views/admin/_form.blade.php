93e9eceb1f90dd5c54f9eaece984e3d08d7fb0e3
Samuel De Backer <sdebacker@gmail.com>
2021-01-05 11:10:59 +0100

    BS5

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -12,14 +12,14 @@
 <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
 <files-field :init-files="{{ $model->files }}"></files-field>
 
-<div class="form-row">
+<div class="row gx-3">
     <div class="col-sm-6">
         {!! BootForm::date(__('Date'), 'date')->value(old('date') ? : $model->present()->dateOrNow('date'))->addClass('datepicker')->required() !!}
     </div>
 </div>
 
 @include('core::form._title-and-slug')
-<div class="form-group">
+<div class="mb-3">
     {!! TranslatableBootForm::hidden('status')->value(0) !!}
     {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
 </div>
