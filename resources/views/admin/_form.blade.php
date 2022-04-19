4f5f6e24271d77010aa714bdb69b1eb95daa1345
Samuel De Backer <sdebacker@gmail.com>
2022-04-19 21:35:11 +0200

    forms

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -3,25 +3,31 @@
     <script src="{{ asset('components/ckeditor4/config-full.js') }}"></script>
 @endpush
 
-@component('core::admin._buttons-form', ['model' => $model])
-@endcomponent
+<div class="header">
+    @include('core::admin._button-back', ['url' => $model->indexUrl(), 'title' => __('News')])
+    @include('core::admin._title', ['default' => __('New news')])
+    @component('core::admin._buttons-form', ['model' => $model])
+    @endcomponent
+</div>
 
-{!! BootForm::hidden('id') !!}
+<div class="content">
+    {!! BootForm::hidden('id') !!}
 
-<file-manager></file-manager>
-<file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-<files-field :init-files="{{ $model->files }}"></files-field>
+    <file-manager></file-manager>
+    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+    <files-field :init-files="{{ $model->files }}"></files-field>
 
-<div class="row gx-3">
-    <div class="col-sm-6">
-        {!! BootForm::date(__('Date'), 'date')->value(old('date') ? : $model->present()->dateOrNow('date'))->addClass('datepicker')->required() !!}
+    <div class="row gx-3">
+        <div class="col-sm-6">
+            {!! BootForm::date(__('Date'), 'date')->value(old('date') ? : $model->present()->dateOrNow('date'))->addClass('datepicker')->required() !!}
+        </div>
     </div>
-</div>
 
-@include('core::form._title-and-slug')
-<div class="mb-3">
-    {!! TranslatableBootForm::hidden('status')->value(0) !!}
-    {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+    @include('core::form._title-and-slug')
+    <div class="mb-3">
+        {!! TranslatableBootForm::hidden('status')->value(0) !!}
+        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+    </div>
+    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+    {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
 </div>
-{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -4,11 +4,6 @@
 
 @section('content')
 
-    <div class="header">
-        @include('core::admin._button-back', ['module' => 'news'])
-        <h1 class="header-title">@lang('New news')</h1>
-    </div>
-
     {!! BootForm::open()->action(route('admin::index-news'))->multipart()->role('form') !!}
         @include('news::admin._form')
     {!! BootForm::close() !!}
