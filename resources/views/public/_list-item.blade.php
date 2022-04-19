d4b5c20e0de38a717325112090def9891b4c02ec
Samuel De Backer <sdebacker@gmail.com>
2022-04-19 21:35:07 +0200

    img size

--- resources/views/public/_list-item.blade.php
+++ resources/views/public/_list-item.blade.php
@@ -1,7 +1,7 @@
 <li class="news-list-item">
     <a class="news-list-item-link" href="{{ $news->uri() }}">
         @empty (!$news->image)
-        <img class="news-list-item-image" src="{{ $news->present()->image(540, 400) }}" width="{{ $news->image->width }}" height="{{ $news->image->height }}" alt="{{ $news->image->alt_attribute }}">
+        <img class="news-list-item-image" src="{{ $news->present()->image(600, 400) }}" width="300" height="200" alt="{{ $news->image->alt_attribute }}">
         @endempty
         <div class="news-list-item-info">
             <h2 class="news-list-item-title">{{ $news->title }}</h2>
