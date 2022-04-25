cd7af1d6988f0893ab0414da09d2a4c9f7c9bd6e
Samuel De Backer <sdebacker@gmail.com>
2022-04-25 11:34:23 +0200

    Update ModuleServiceProvider.php

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -20,7 +20,7 @@ class ModuleServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'news');
 
         $this->publishes([__DIR__.'/../../database/migrations/create_news_table.php.stub' => getMigrationFileName('create_news_table')], 'typicms-migrations');
-        $this->publishes([__DIR__.'/../../resources/views' => resource_path('views/vendor/news')], 'typicms-views');
+        $this->publishes([__DIR__.'/../../resources/views' => resource_path('views/news')], 'typicms-views');
         $this->publishes([__DIR__.'/../../resources/scss' => resource_path('scss')], 'typicms-resources');
 
         AliasLoader::getInstance()->alias('News', NewsFacade::class);
