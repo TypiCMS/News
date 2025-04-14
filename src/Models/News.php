511fffaed9de2e319e94301b5c727e0acc4719b6
Samuel De Backer <sdebacker@gmail.com>
2025-04-14 15:09:24 +0200

    casts() method

--- src/Models/News.php
+++ src/Models/News.php
@@ -24,9 +24,15 @@ class News extends Base implements Feedable
 
     protected string $presenter = ModulePresenter::class;
 
-    protected $casts = [
-        'date' => 'datetime:Y-m-d',
-    ];
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'date' => 'datetime:Y-m-d',
+        ];
+    }
 
     protected $guarded = [];
 
