d619200443e0380ba75050d28da6b069d8f4febb
Samuel De Backer <sdebacker@gmail.com>
2025-08-18 16:25:07 +0200

    return type fixed

--- src/Exports/Export.php
+++ src/Exports/Export.php
@@ -3,7 +3,6 @@
 namespace TypiCMS\Modules\News\Exports;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
@@ -21,7 +20,7 @@ use TypiCMS\Modules\News\Models\News;
  */
 class Export implements FromCollection, ShouldAutoSize, WithColumnFormatting, WithHeadings, WithMapping
 {
-    /** @return Collection<int, Model> */
+    /** @return Collection<int, News> */
     public function collection(): Collection
     {
         return QueryBuilder::for(News::class)
