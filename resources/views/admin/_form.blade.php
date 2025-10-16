0cba2b6bccac0384ef3db59adaab0a0c0c9d8762
Samuel De Backer <sdebacker@gmail.com>
2025-10-16 13:05:37 +0200

    Update _form.blade.php

--- resources/views/admin/_form.blade.php
+++ resources/views/admin/_form.blade.php
@@ -7,22 +7,29 @@
 <div class="content">
     <x-core::form-errors />
 
-    <file-manager></file-manager>
-    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-    <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
-    <files-field :init-files="{{ $model->files }}"></files-field>
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="row gx-3">
+                <div class="col-sm-6">
+                    {!! BootForm::date(__('Date'), 'date')->value(old('date') ?: $model->present()->dateOrNow('date'))->addClass('datepicker')->required() !!}
+                </div>
+            </div>
 
-    <div class="row gx-3">
-        <div class="col-sm-6">
-            {!! BootForm::date(__('Date'), 'date')->value(old('date') ?: $model->present()->dateOrNow('date'))->addClass('datepicker')->required() !!}
+            <x-core::title-and-slug-fields :locales="locales()" />
+            <div class="mb-3">
+                {!! TranslatableBootForm::hidden('status')->value(0) !!}
+                {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+            </div>
+            {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+            <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
+        </div>
+        <div class="col-lg-4">
+            <div class="right-column">
+                <file-manager></file-manager>
+                <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+                <file-field type="image" field="og_image_id" :init-file="{{ $model->ogImage ?? 'null' }}" label="Open Graph image"></file-field>
+                <files-field :init-files="{{ $model->files }}"></files-field>
+            </div>
         </div>
     </div>
-
-    <x-core::title-and-slug-fields :locales="locales()" />
-    <div class="mb-3">
-        {!! TranslatableBootForm::hidden('status')->value(0) !!}
-        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-    </div>
-    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-    <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
 </div>
