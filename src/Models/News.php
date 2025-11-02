a4cb5bb00e9c5ae6a4d6e23648e794240771da32
Samuel De Backer <sdebacker@gmail.com>
2025-11-02 19:35:34 +0100

    Spatie\Translatable replaced by TypiCMS\Translatable

--- src/Models/News.php
+++ src/Models/News.php
@@ -10,13 +10,13 @@ use Illuminate\Support\Facades\Route;
 use Laracasts\Presenter\PresentableTrait;
 use Spatie\Feed\Feedable;
 use Spatie\Feed\FeedItem;
-use Spatie\Translatable\HasTranslations;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\Core\Models\File;
 use TypiCMS\Modules\Core\Models\History;
 use TypiCMS\Modules\Core\Traits\HasFiles;
 use TypiCMS\Modules\Core\Traits\Historable;
 use TypiCMS\Modules\News\Presenters\ModulePresenter;
+use TypiCMS\Translatable\HasTranslations;
 
 /**
  * @property int $id
